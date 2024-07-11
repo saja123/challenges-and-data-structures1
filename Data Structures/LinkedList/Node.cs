c352e9f14924e1c4c4cd8e599686f398abf114f6
saja123 <saja_fawaz@yahoo.com>
2024-07-11 22:35:58 +0300

    solve Linkid-List

---
+++ Data Structures/LinkedList/Node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace challenges_and_data_structures1
+{
+    public class Node
+    {
+        public int Data { get; set; }
+        public Node Next { get; set; }
+        public Node(int data)
+        {
+            Data = data; // 5 -Next-> 10  NodeA = NodeB
+            Next = null;
+        }
+    }
+}
