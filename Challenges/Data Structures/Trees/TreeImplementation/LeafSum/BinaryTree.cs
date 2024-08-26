60d28877c76bcf00d4bd8f573180b5701e11ec5c
saja123 <saja_fawaz@yahoo.com>
2024-08-26 14:02:55 +0300

    Solve LargestLevel

--- Challenges/Data Structures/Trees/TreeImplementation/LeafSum/BinaryTree.cs
+++ Challenges/Data Structures/Trees/TreeImplementation/LeafSum/BinaryTree.cs
@@ -1,67 +1,113 @@
 ﻿using System;
 using System.Collections.Generic;
-
-namespace TreeImplementation.LeafSum
+public class BinaryTree
 {
-    public class BinaryTree
+    public Node Root;
+    
+
+    public List<int> LargestValueEachLevel()
     {
-        public Node Root { get; set; }
+        List<int> largestValues = new List<int>();
+        if (Root == null) return largestValues;
 
-        public BinaryTree()
+        Queue<Node> queue = new Queue<Node>();
+        queue.Enqueue(Root);
+
+        while (queue.Count > 0)
         {
-            Root = null;
-        }
+            int levelSize = queue.Count;
+            int maxAtLevel = int.MinValue;
 
-        public BinaryTree(int value)
-        {
-            Root = new Node(value);
-        }
-
-        public int SumOfLeafNodes()
-        {
-            return CalculateLeafSum(Root);
-        }
-
-        private int CalculateLeafSum(Node node)
-        {
-            if (node == null)
-                return 0;
-
-            if (node.Left == null && node.Right == null)
-                return node.Value;
-
-            int leftSum = CalculateLeafSum(node.Left);
-            int rightSum = CalculateLeafSum(node.Right);
-
-            return leftSum + rightSum;
-        }
-
-        public void Print()
-        {
-            Print(Root, "", true);
-        }
-
-        private void Print(Node node, string indent, bool isLast)
-        {
-            if (node != null)
+            for (int i = 0; i < levelSize; i++)
             {
-                Console.Write(indent);
-                if (isLast)
+                Node current = queue.Dequeue();
+                if (current.Value > maxAtLevel)
                 {
-                    Console.Write("└─");
-                    indent += "  ";
-                }
-                else
-                {
-                    Console.Write("├─");
-                    indent += "| ";
+                    maxAtLevel = current.Value;
                 }
 
-                Console.WriteLine(node.Value);
+                if (current.Left != null)
+                {
+                    queue.Enqueue(current.Left);
+                }
 
-                Print(node.Left, indent, node.Right == null);
-                Print(node.Right, indent, true);
+                if (current.Right != null)
+                {
+                    queue.Enqueue(current.Right);
+                }
             }
+            largestValues.Add(maxAtLevel);
         }
+
+        return largestValues;
     }
 }
+
+
+//using System;
+//using System.Collections.Generic;
+
+//namespace TreeImplementation.LeafSum
+//{
+//    public class BinaryTree
+//    {
+//        public Node Root { get; set; }
+
+//        public BinaryTree()
+//        {
+//            Root = null;
+//        }
+
+//        public BinaryTree(int value)
+//        {
+//            Root = new Node(value);
+//        }
+
+//        public int SumOfLeafNodes()
+//        {
+//            return CalculateLeafSum(Root);
+//        }
+
+//        private int CalculateLeafSum(Node node)
+//        {
+//            if (node == null)
+//                return 0;
+
+//            if (node.Left == null && node.Right == null)
+//                return node.Value;
+
+//            int leftSum = CalculateLeafSum(node.Left);
+//            int rightSum = CalculateLeafSum(node.Right);
+
+//            return leftSum + rightSum;
+//        }
+
+//        public void Print()
+//        {
+//            Print(Root, "", true);
+//        }
+
+//        private void Print(Node node, string indent, bool isLast)
+//        {
+//            if (node != null)
+//            {
+//                Console.Write(indent);
+//                if (isLast)
+//                {
+//                    Console.Write("└─");
+//                    indent += "  ";
+//                }
+//                else
+//                {
+//                    Console.Write("├─");
+//                    indent += "| ";
+//                }
+
+//                Console.WriteLine(node.Value);
+
+//                Print(node.Left, indent, node.Right == null);
+//                Print(node.Right, indent, true);
+//            }
+//        }
+//    }
+//}
